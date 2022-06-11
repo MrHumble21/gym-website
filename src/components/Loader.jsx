1b9b81a405025438c9f17d463c8a95e0443634dc
Abdulboriy <malikov.udemy@gmail.com>
2022-06-12 01:31:17 +0500

    commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
+import { CirclesWithBar } from 'react-loader-spinner';
 
-const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="start" width="100%">
+    <CirclesWithBar color="red" />
+  </Stack>
+);
 
-export default Loader
+export default Loader;
