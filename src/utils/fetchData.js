351cf1845e1b5cb17869cc4746f1a83cb09a5a87
Abdulboriy <malikov.udemy@gmail.com>
2022-06-12 01:31:36 +0500

    last commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,14 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "39d4db584bmshe48255d07f645e9p114acajsnf4c6801119af",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
